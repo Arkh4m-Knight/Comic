19323790a127925bf415d0cf3df4fa7303da8e6e
Arkh4m-Knight <sudhakarzxz@gmail.com>
2025-10-21 22:25:17 +0530

    Update types.ts

--- comicmob/src/types.ts
+++ comicmob/src/types.ts
@@ -1,4 +1,4 @@
-export type Genre = "Action" | "Fantasy" | "Sci-Fi" | "Romance" | "Comedy" | "Drama";
+export type Genre = "Action" | "Fantasy" | "Sci-Fi" | "Romance" | "Comedy" | "Drama" | "Adventure";
 export type ComicFormat = "Comic" | "Manga" | "Manhwa";
 
 export interface Episode {
